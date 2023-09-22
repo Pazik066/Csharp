629be40a46ffb730edaaf24946ed25b6140382f2
pazik <pazik066@mail.ru>
2023-09-22 11:29:01 +0300

    completed the Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// 1. на вход принимает число и 
+// 2. выдыдаёт его квадрат. 
+// 3. вывод результата
+// Например: 4 -> 16; -3 -> 9; -7 -> 49.
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+//Console.WriteLine(number + " -> " + squre);
+Console.WriteLine($"{number} -> {square}");
