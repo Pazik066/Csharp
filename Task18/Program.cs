027408db57ce2ebf8ce29f0eda5365bd3b6599ad
pazik <pazik066@mail.ru>
2023-10-02 00:59:19 +0300

    Finish seminar

---
+++ Task18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат в этой четверти (x и y).
+
+
+using System.Runtime.Intrinsics.X86;
+
+string GetCoordinatesRange(int num) // Метод
+{
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x < 0, y > 0";
+    if (num == 3) return "x < 0, y < 0";
+    if (num == 4) return "x > 0, y < 0"; // Условия
+    return null; // Возврат нуля при невыполнении ни одного условия
+}
+
+Console.WriteLine("Введите номер четверти"); // Запрос на ввод координат
+int quater = Convert.ToInt32(Console.ReadLine()); // Конвертация
+
+
+string getCoordinatesRange = GetCoordinatesRange(quater); // Вызов метода
+string result = getCoordinatesRange != null ? $"Заданному номеру четверти, соответсвует диапазон: {getCoordinatesRange}" : "Введены некорректные координаты"; // Тернарный оператор, создаем переменную
+                                                                            // Если больше нуля ? тогда формируем строку для вывода переменной : иначе возвращаем строку
+
+Console.WriteLine(getCoordinatesRange);
---
+++ Task20/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 20. Напишите программу, которая принимает на вход координаты двух точек и находи расстояние между ними в 2D пространстве.
+// А (3, 6); В (2, 1) -> 5,09
+// A (7;-5); B (1,-1) -> 7,21
+
+// Math.Pow(5, 6); // вызов метода, который возводит число в степень, возвращает тип данных Double.
+// Math.Sqrt(5); // вызов метода , который извлекает корень, возвращает тип данных Double.
+
+// double d = 5.09953345; // Вводим переменную
+// double dRound = Math.Round(d, 2, MidpointRounding.ToZero); // округление с использованием еще отдного метода
+//                                                             // (переменная, количество чисел, стратегия округления)
+
+// System.Console.WriteLine(dRound);
+
+double FindDistace(int ax, int ay, int bx, int by)
+{
+    int leg1 = ax - bx;
+    int leg2 = ay - by;
+    double distance = Math.Sqrt(leg1 * leg1 + leg2 * leg2);
+    return distance;
+}
+
+Console.WriteLine("Введите координаты точки A");
+Console.Write("X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B");
+Console.Write("X: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = FindDistace(x1 , y1, x2, y2);
+double resultRound = Math.Round(result, 2, MidpointRounding.ToZero);
+Console.WriteLine(resultRound);
+
+Console.WriteLine($"{d:F2}"); // Способ округления без стратегий. {переменная, количество знаков};
