1070e137cac7c716da7fd6dadb0c7283d079722f
pazik <pazik066@mail.ru>
2023-10-04 20:57:53 +0300

    Finish Home work

---
+++ Task19_HW/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+        string Number = Convert.ToString(number);
+        if(Number.Length !=5)
+        {
+            Console.WriteLine("Число не пятизначное");
+            return false;
+        }
+      
+        return Number[0] == Number[4] && Number[1] == Number[3];
+      
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64546;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
---
+++ Task21_HW/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D простнранстве.
+
+using System;
+
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+      // Введите свое решение ниже
+      int leg1 = pointA[0] - pointB[0];
+      int leg2 = pointA[1] - pointB[1];
+      int leg3 = pointA[2] - pointB[2];
+      double distance = Math.Sqrt(leg1 * leg1 + leg2 * leg2 + leg3 * leg3);
+      return distance;
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
